c931590d02f4975545469bb5c0b8d7191ea59fd1
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-10-28 13:32:45 +0500

    conversion datatype

---
+++ basic_js/conversionOperation.js
@@ -0,0 +1,29 @@
+let score= "wahab"
+console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => convert string into number 33
+// "33abc" => give NaN(means not a number)
+// true => 1 ; false => 0;
+// "wahab" => NaN( means not a number)
+
+let isLoggedIn = "wahab";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1=>true ; 0=>false;
+// ""=>false;
+// "wahab"=>true;
+
+let someNumber = 33
+let srtingsomeNumber = String(someNumber);
+console.log(srtingsomeNumber);
+console.log(typeof srtingsomeNumber);
+
+
+
