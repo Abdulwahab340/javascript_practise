38112f2e55dd0cdc5695de4f93b54f71d6aa778b
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-10-28 16:36:18 +0500

    compersison

--- basic_js/conversionOperation.js
+++ basic_js/conversionOperation.js
@@ -1,10 +1,10 @@
 let score= "wahab"
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => convert string into number 33
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "wahab";
 let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1=>true ; 0=>false;
 // ""=>false;
@@ -22,8 +22,39 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 33
 let srtingsomeNumber = String(someNumber);
-console.log(srtingsomeNumber);
-console.log(typeof srtingsomeNumber);
+// console.log(srtingsomeNumber);
+// console.log(typeof srtingsomeNumber);
 
+// **********************************Operations********************************************************
+let value = 3
+let negvalue = -value
+//  console.log(negvalue + '----------------');
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3)
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Rana"
+let str2 = " Wahab"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// Code ko professional way ma likhta ha like () in paranthesis ka use krna
+// console.log((3+4)*5%3);
+// console.log(true);
+
+let num1,num2,num3
+num1=num2=num3=2+2  // is ma wo num1 num2 aur num3 ko 4 value assign kr rha ha lkin ya tareeka thek ni ha is sa apko tw samj aarae ha but samna wala ko nhi
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
