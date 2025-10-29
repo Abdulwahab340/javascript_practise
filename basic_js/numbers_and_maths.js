82119b63e8d95b60912ec6447b8f6477e122cc8e
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-10-29 17:14:21 +0500

    Number_Math_Summary

---
+++ basic_js/numbers_and_maths.js
@@ -0,0 +1,36 @@
+// *************************************************************************************************************************::::::::NUMBERS:::::::::::::::: *********************************************
+
+ const score = 400
+
+const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8977
+// console.log(otherNumber.toPrecision(3));
+
+const secondNumber = 100000000
+// console.log(secondNumber.toLocaleString('en-IN'));
+
+// ***********************************************************************************************************************:::::::::::::::::MATHS::::::::::::::*********************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+console.log((Math.random()));
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
