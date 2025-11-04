de9d20e9dced9ce084bd5e420bf1eaf8caec7d1b
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-11-04 13:13:18 +0500

    04_iife.js

---
+++ basics_03/04_iife.js
@@ -0,0 +1,16 @@
+(function chai(){
+    //  ya named iffe ha
+    console.log(`DB CONNECTED`);
+})();
+
+
+// **********************
+// (function code (){
+//     console.log(`DB CONNECTED TWO`);
+// })()
+
+// In Arrow function syntax
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('Abdulwahab')
+  
