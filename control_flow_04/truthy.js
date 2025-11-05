ba339088624a38c81b4d40de0bb5ee4530eb0e86
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-11-05 15:46:38 +0500

    truthy_falsy

---
+++ control_flow_04/truthy.js
@@ -0,0 +1,46 @@
+
+// const userEmail = "Rana@gmail.com"
+// const userEmail = ""
+ const userEmail = []
+if (userEmail){
+    console.log("Got User Email");
+}else{
+    console.log("Dont get User Email");
+}
+
+// ::::: Falsy Values :::::
+// false , 0 ,-0 , BigInt  0n , "" , null , undefined , NaN
+
+// :::::Some truthy values::::::::
+// "0" , " " , 'false' , [] , {} , function(){}
+
+// if(userEmail.length===0){
+//     console.log("Array is Empty");
+// }
+
+const emptyobj = {}
+if(Object.keys(emptyobj).length===0){
+    console.log("Object is Empty");
+}
+
+//:::::::::: Nullish Coalescing Operator (??) :::::::::::(null undefined) ::::::::::
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// :::::::::: Terniary Operator:::::::::::::
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less then 80") : console.log("More then 80");
+
+
+
+
+
+
