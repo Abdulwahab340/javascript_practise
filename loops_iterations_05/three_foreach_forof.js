03f4ba2044ce577c415755a7556a1aeb7e79e99d
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-11-06 14:52:30 +0500

    for of loops

---
+++ loops_iterations_05/three_foreach_forof.js
@@ -0,0 +1,32 @@
+// ::::: for of  (loop) :::::
+// ["" , "" ,  ""]
+// [{} , {} , {}]
+const arr = [1 , 2 , 3 , 4 , 5]
+for (const num of arr) {
+    // console.log(num);
+    
+}
+const greetings = " Hello World !"
+for (const greet of greetings) {
+    // console.log(` Each char is : ${greet}`);
+}
+
+// :::::: Maps ::::::::
+const map = new  Map()
+map.set('IN' ,"India")
+map.set('USA' ,"United States Of America")
+map.set('FR' ,"France")
+
+// console.log(map);
+
+for (const [key , value] of map) {
+    // console.log(key , ':-' , value);
+}
+
+const myObj = {
+    game1:'PUBG',
+    game2:'FreeFire'
+}
+// for (const [key,value] of myObj) {
+//     console.log(key,':-',value);
+// }
