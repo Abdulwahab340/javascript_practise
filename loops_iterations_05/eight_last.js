ca8c5e03316178ab0609f555780fce60a6ac9e63
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-11-07 15:29:56 +0500

    eight.js

---
+++ loops_iterations_05/eight_last.js
@@ -0,0 +1,31 @@
+const myNums = [1 , 2 , 3]
+// const mySums = myNums.reduce(function(accumolator,currentvalue){
+//     console.log(`accumolator is ${accumolator} and current value is ${currentvalue}`);
+    
+//     return accumolator+currentvalue
+// },0)
+
+const myTotal = myNums.reduce( (acc,curr) => acc+curr,0)
+console.log(myTotal);
+
+
+const ShopingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+     {
+        itemName : "python course",
+        price : 999
+    },
+     {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+     {
+        itemName : "data science course",
+        price : 12999
+    },
+]
+const pricetopay = ShopingCart.reduce((acc  , item)=>acc + item.price,0)
+console.log(pricetopay);
