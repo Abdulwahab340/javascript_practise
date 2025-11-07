0287e458c2d12d4df865579b892a4982a150c1b1
Abdulwahab340 <ranaabdulwahab793@gmail.com>
2025-11-07 12:48:27 +0500

    six.js

---
+++ loops_iterations_05/six.js
@@ -0,0 +1,39 @@
+// const coding = [ "js","ruby","java","python","Cpp"]
+// const values = coding.forEach((item) => {
+//  console.log(item);
+// return item
+// })
+// console.log(values);
+
+
+
+// const myNums = [ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+//  let NewNum=myNums.filter((num)=>num>4)
+//  console.log(NewNum);
+
+
+
+// const myNums = [ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+//  let NewNum=myNums.filter((num)=>{
+//   return  num>4
+// })
+//  console.log(NewNum);
+ 
+
+
+// const myNums = [ 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+// const newNums = []
+// myNums.forEach((num)=> {
+//     if(num>4)
+//     {
+// newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
+
+
+
+
+
